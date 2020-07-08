ab94f517edf29ac07e3594343697ba79e6a79812
Vladimir <vkuznetsov@exadel.com>
2020-07-08 17:26:48 +0300

    New projects

---
+++ MVVM.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MVVM.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
